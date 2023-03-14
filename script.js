11018903a434d5513107ef5aef2b801795cf2833
Michael <mherman315@gmail.com>
2023-03-13 20:16:44 -0700

    Dotted my T's and Crossed my Eyes. I finally got the navbar to align right and added some color. Also change some HTML in about.html

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const links = document.querySelectorAll('a');
-const colors = {
-  green: '#042a2bff',
-  moonstone: '#5eb1bfff',
-  light: '#cdedf6ff',
-  gold: '#e2c044ff',
-  sinopia: '#cc3f0cff',
-};
-
 // for (let i = 0; i < links.length; i++) {
 //   links[i].addEventListener('mouseover', function (event) {
 //     event.target.style.color = colors.gold;
